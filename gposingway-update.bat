c40f85c1b2941d31e6a2ca6618cf38c8e103ebc2
Leon Aquitaine <leonaquitaine@gmail.com>
2024-10-11 09:19:27 -0400

    Nyanya.studio (#47)
    * Added Nyanya.studio preset collection
    * Added guardrails for insufficient permissions on Win11

--- gposingway-update.bat
+++ gposingway-update.bat
@@ -3,6 +3,7 @@ cls
 setlocal enabledelayedexpansion
 setlocal ENABLEEXTENSIONS
 
+:: 2024/09/07   1.0.5   Added guardrails for insufficient permissions on Win11.
 :: 2024/06/21   1.0.4   Added guardrails for deprecation list handler.
 :: 2024/06/09   1.0.3   Added ReShade.ini to the list of copied items for backup.
 :: 2024/06/08   1.0.2   FIX - replaced BITSADMIN with powershell for file transfer
@@ -23,7 +24,7 @@ for /f "tokens=1-6 delims=/: " %%a in ('robocopy "|" . /njh /ndl ^| find ":"') d
 echo ------------------------------------------------
 echo  (\(\
 echo  ( o.o)    GPosingway Update/Installer Tool
-echo  O_(")(")
+echo  O_(")(")  1.0.5
 echo ------------------------------------------------
 echo.
 echo Welcome to the GPosingway Installer^^!
@@ -35,6 +36,27 @@ if not exist ffxiv_dx11.exe (
     echo.
     echo Please make sure this .BAT file is placed in your FFXIV game directory:
     echo "[...]\SquareEnix\FINAL FANTASY XIV - A Realm Reborn\game"
+    pause
+    exit /b 1
+)
+
+:: Check if the batch file has permission to write to the current directory
+echo write-test > gposingway.temp 2>nul
+
+rem Check if the file was created
+if exist gposingway.temp (
+  del gposingway.temp
+) else (
+    echo.
+    echo WARNING: It appears I don't have permission to write to the game directory.
+    echo This is necessary to download and install GPosingway components.
+    echo.
+    echo To fix this issue, please follow the instructions here:
+    echo  https://github.com/gposingway/gposingway/blob/main/md/troubleshooting.md#not-enough-permissions
+    echo.
+    echo Alternatively, you can manually download and install GPosingway:
+    echo  https://github.com/gposingway/gposingway/blob/main/md/gposingway_installation.md
+    echo.
     pause
     exit /b 1
 )
